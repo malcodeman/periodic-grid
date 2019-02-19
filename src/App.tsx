437cedf8d7b5ab9605e997a25b85c10615bf3ab3
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-02-19 23:06:10 +0100

    Arranges elements correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
 import style from "./App.module.css";
-import elements from "./elements.json";
+import elements from "./data/elements.json";
+import lanthanoids from "./data/lanthanoids.json";
+import actinides from "./data/actinides.json";
 
 class App extends Component<{}, {}> {
   render() {
@@ -11,13 +13,45 @@ class App extends Component<{}, {}> {
         <div className={style.table}>
           {elements.map(element => {
             return (
-              <div className={style.element}>
+              <div
+                className={style.element}
+                data-element={element.number}
+                key={element.number}
+              >
                 <span className={style.number}>{element.number}</span>
                 <span className={style.symbol}>{element.symbol}</span>
                 <span className={style.name}>{element.name}</span>
               </div>
             );
           })}
+          {lanthanoids.map(element => {
+            return (
+              <div
+                className={style.element}
+                data-element={element.number}
+                key={element.number}
+              >
+                <span className={style.number}>{element.number}</span>
+                <span className={style.symbol}>{element.symbol}</span>
+                <span className={style.name}>{element.name}</span>
+              </div>
+            );
+          })}
+          {actinides.map(element => {
+            return (
+              <div
+                className={style.element}
+                data-element={element.number}
+                key={element.number}
+              >
+                <span className={style.number}>{element.number}</span>
+                <span className={style.symbol}>{element.symbol}</span>
+                <span className={style.name}>{element.name}</span>
+              </div>
+            );
+          })}
+          <div className={style.lanthanoids}>lanthanoids</div>
+          <div className={style.actinides}>actinides</div>
         </div>
       </div>
     );
