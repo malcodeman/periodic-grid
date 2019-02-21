b671b5f6afd94f72617fbbbd851048299469722a
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-02-21 22:41:47 +0100

    Improves styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,34 @@ import style from "./App.module.css";
 import elements from "./data/elements.json";
 
 class App extends Component<{}, {}> {
+  getStyle = (category: string): string => {
+    switch (category) {
+      case "diatomic nonmetal":
+        return `${style.element} ${style.halogenCategory}`;
+      case "noble gas":
+        return `${style.element} ${style.nobleGasCategory}`;
+      case "alkali metal":
+        return `${style.element} ${style.alkaliMetalCategory}`;
+      case "alkaline earth metal":
+        return `${style.element} ${style.alkalineEarthMetalCategory}`;
+      case "metalloid":
+        return `${style.element} ${style.metalloidCategory}`;
+      case "polyatomic nonmetal":
+        return `${style.element} ${style.otherNonmetalCategory}`;
+      case "post-transition metal":
+        return `${style.element} ${style.postTransitionMetalCategory}`;
+      case "lanthanide":
+        return `${style.element} ${style.lanthanideCategory}`;
+      case "actinide":
+        return `${style.element} ${style.actinidesCategory}`;
+      case "transition metal":
+        return `${style.element} ${style.transitionMetalCategory}`;
+      case "unknown":
+        return `${style.element} ${style.unknownCategory}`;
+      default:
+        return style.element;
+    }
+  };
   render() {
     return (
       <div className={style.container}>
@@ -12,13 +40,18 @@ class App extends Component<{}, {}> {
           {elements.map(element => {
             return (
               <div
-                className={style.element}
+                className={this.getStyle(element.category)}
                 data-element={element.number}
                 key={element.number}
               >
-                <span className={style.number}>{element.number}</span>
-                <span className={style.symbol}>{element.symbol}</span>
                 <span className={style.name}>{element.name}</span>
+                <span className={style.symbol}>{element.symbol}</span>
+                <footer className={style.footer}>
+                  <span className={style.atomicMass}>
+                    {element.atomic_mass.toLocaleString()}
+                  </span>
+                  <span className={style.number}>{element.number}</span>
+                </footer>
               </div>
             );
           })}
